8cff776996c049546c6659f93fea94cdf87751ec
autumn-ragland <raglanda998@gmail.com>
2020-12-15 18:59:17 -0600

    walkthrough

--- main.js
+++ main.js
@@ -1,37 +1,41 @@
 console.log("20 12 14 JS Classes 01 Practice"); // files linked
 
-// movie class with properties name, rating, and year released
+// Declare a Movie class with properties name, rating, and year released. 
 class Movie {
-    constructor(name, rating, yearReleased){
-        this.name = name;
+    // Set the value of this properties for each instance in the constructor.
+    constructor(movieName, rating, yearReleased){
+        this.movieName = movieName;
         this.rating = rating;
         this.yearReleased = yearReleased;
     }
-    // CLASS METHODS
-    // output all movie properties in a formatted string
+    // The Movie class will also have three methods : outputMovie, changeRating and changeYearReleased. 
+    // The outputMovie method will output all movie properties in a formatted string
     outputMovie(){
-        console.log(`The movie ${this.name} came out in ${this.yearReleased} and has a rating of ${this.rating}`);
+        console.log(`The movie ${this.movieName} came out in ${this.yearReleased} and has a rating of ${this.rating}`);
     }
-    // accept a new rating and update the rating property
+    // The changeRating method will accept a new rating an update the rating property of a Movie object.
     changeRating(newRating){
         this.rating = newRating;
+        return(`The updated rating is ${this.rating}`);
     }
-    // accept a new year released and update the year released property\
+    // The changeYearReleased method will accept a new year released and update the year released property of a Movie object.
     changeYear(newYear){
         this.yearReleased = newYear;
     }
 }
-
-// Create two instances of the movie class with different property values. 
-let favMovie = new Movie("The Discovery", "5 Stars", "2019");
-let secondFavMovie = new Movie("Freak", "3 stars", "2017");
-
-// // Call the outputMovie method on both movies.
+// Create two instances of the movie class with different property values.
+let favMovie = new Movie("The Discovery", 5, 2020);
+// console.log(favMovie);
 favMovie.outputMovie();
+
+// second Movie object
+let secondFavMovie = new Movie("Freak", 3, 2019);
 secondFavMovie.outputMovie();
 
-// Update the rating and the year released
-favMovie.changeYear("2020");
+// Update the rating 
+console.log(favMovie.changeRating(4));
+
+ // update year
+favMovie.changeYear(2019);
 favMovie.outputMovie();
-favMovie.changeRating("4 stars");
-favMovie.outputMovie();
+
